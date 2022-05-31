98d986e9ad0d7698b50363f23c625fa6327a8845
Contemporaneamente <54739919+Contemporaneamente@users.noreply.github.com>
2022-05-31 10:59:35 +0200

    aggiunte alcune funzioni e sistemazione generale

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ screen = pygame.display.set_mode(size)
 point1 = po.pointMass(10, 200, 200, screen) 
 point2 = po.pointMass(10, 300, 300, screen) 
 point3 = po.pointMass(10, 200, 300, screen) 
+point4 = po.pointMass(10, 300, 200, screen) 
 
-polyTest = po.polygon([point1, point2, point3], screen)
+polyTest = po.polygon([point1, point2, point3, point4], screen)
+rPolyTest = po.regularPolygon((200,200), 11, 50, screen)
 
 rbTest = po.rigidBody(po.polygon([point1, point2], screen))
 
@@ -25,7 +27,7 @@ while render:
     
     screen.fill((252,252,253))
 
-    polyTest.draw()
+    rPolyTest.draw()
 
     pygame.draw.rect(screen, (50,50,50), (0, height - 30, width, 30))
     pygame.display.flip()
